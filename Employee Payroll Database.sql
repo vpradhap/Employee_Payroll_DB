c0204646ccdc9909990767899f87d028042e46db
vpradhap <pradhapvenkat@gmail.com>
2022-06-16 22:26:50 +0530

    Creating table

--- Employee Payroll Database.sql
+++ Employee Payroll Database.sql
@@ -1,3 +1,5 @@
 --UC1 Creating database
 create database Employee_Payroll_DB
 use Employee_Payroll_DB
+--UC2 Createing table
+create table Employee_payroll(id int identity(1,1) primary key,name varchar(200),salary int,joining_date date) 
